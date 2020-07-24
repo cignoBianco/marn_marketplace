54655333d15cf7987f5d03e97a9b919b7db59e57
cignobianco <smiana123@gmail.com>
2020-07-24 12:06:53 +0700

    1206

--- client/main.js
+++ client/main.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import { hydrate } from 'react-dom'
-import App from './App'
-import "./public/css/css.module.css"
+import React from 'react';
+import { hydrate } from 'react-dom';
+import App from './App';
+import "./public/css/style.css";
+import "./public/css/styles__ltr.css";
+import "./public/css/font-awesome-css.min.css";
+import "./public/css/styles.e7726758f4812a2ea370.css";
 
 
-hydrate(<App/>, document.getElementById('root'))
+hydrate(<App/>, document.getElementById('root'));
--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -23,11 +23,11 @@ const config = {
             {
                 test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
                 use: 'file-loader'
-            },
+            },/*
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-            }
+            }*/
         ]
     }
 }
