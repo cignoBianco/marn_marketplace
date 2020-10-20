16b14e6e9766d8879cdd005396e230aef01de4c2
ana <smiana123@gmail.com>
2020-10-20 03:40:58 +0000

    rename profile

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -272,11 +272,11 @@ const classes = useStyles()
             </>
           )}
           <Link to={"/user/" + auth.isAuthenticated().user._id}>
-            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>Личный кабинет</Button>
           </Link>
           <Button color="inherit" onClick={() => {
               auth.clearJWT(() => history.push('/'))
-            }}>Sign out</Button>
+            }}>Выйти</Button>
         </span>)
       }
     </div>
@@ -288,61 +288,3 @@ const classes = useStyles()
 )})
 
 export default Menu
-
-/*
-
-    <Toolbar>
-      <Typography variant="h6" color="inherit">
-        MERN Marketplace
-      </Typography>
-      <div>
-        <Link to="/">
-          <IconButton aria-label="Home" style={isActive(history, "/")}>
-            <HomeIcon/>
-          </IconButton>
-        </Link>
-        <Link to="/shops/all">
-          <Button style={isActive(history, "/shops/all")}>All Shops</Button>
-        </Link>
-        <Link to="/auctions/all">
-          <Button style={isActive(history, "/auctions/all")}>All Auctions</Button>
-        </Link>
-        <Link to="/cart">
-          <Button style={isActive(history, "/cart")}>
-            Cart
-            <Badge invisible={false} color="secondary" badgeContent={cart.itemTotal()} style={{'marginLeft': '7px'}}>
-              <CartIcon />
-            </Badge>
-          </Button>
-        </Link>      
-      </div>
-      <div style={{'position':'absolute', 'right': '10px'}}><span style={{'float': 'right'}}>
-      {
-        !auth.isAuthenticated() && (<span>
-          <Link to="/signup">
-            <Button style={isActive(history, "/signup")}>Sign up
-            </Button>
-          </Link>
-          <Link to="/signin">
-            <Button style={isActive(history, "/signin")}>Sign In
-            </Button>
-          </Link>
-        </span>)
-      }
-      {
-        auth.isAuthenticated() && (<span>
-          {auth.isAuthenticated().user.seller && (<>
-            <Link to="/seller/shops"><Button style={isPartActive(history, "/seller/")}>My Shops</Button></Link>
-            <Link to="/myauctions"><Button style={isPartActive(history, "/myauctions")}>My Auctions</Button></Link>
-            </>
-          )}
-          <Link to={"/user/" + auth.isAuthenticated().user._id}>
-            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
-          </Link>
-          <Button color="inherit" onClick={() => {
-              auth.clearJWT(() => history.push('/'))
-            }}>Sign out</Button>
-        </span>)
-      }
-      </span></div>
-    </Toolbar> */
